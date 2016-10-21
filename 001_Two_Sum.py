8f43b377f87ba44bc443b39e0b8d8bdeace41c73
Jake <jake_hoare@hotmail.com>
2016-10-22 10:45:44 +1100

    Creation

---
+++ 001_Two_Sum.py
@@ -0,0 +1,30 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/two-sum/
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+# You may assume that each input would have exactly one solution.
+
+# Maintain a mapping from each number to its index and check if the complement has already been found.
+# Time - O(n)
+# Space - O(n) for the dictionary
+
+# An alternative method would be to sort the list then binary search for each target-num.
+# Which could take O(nlogn) time and O(1) space
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        num_to_index = {}           # key is number, value is index in nums
+
+        for i, num in enumerate(nums):
+
+            if target - num in num_to_index:
+                return [i, num_to_index[target - num]]
+
+            num_to_index[num] = i
