41351472127b637fc71a6b1caa9da4c054e1996d
Jake <jake_hoare@hotmail.com>
2016-10-22 13:06:22 +1100

    Add comments

--- 003_Longest_Substring_Without_Repeating_Characters.py
+++ 003_Longest_Substring_Without_Repeating_Characters.py
@@ -4,9 +4,9 @@ _project_ = 'leetcode'
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 # Given a string, find the length of the longest substring without repeating characters.
 
-# TODO SOLUTION
-# Time - O(TODO)
-# Space - O(TODO)
+# Maintain a sliding window, updating the start whenever we see a character repeated.
+# Time - O(n)
+# Space - O(1), dictionary is limited by fixed size alphabet
 
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
@@ -14,11 +14,17 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        last_seen = {}
-        start = 0
+        last_seen = {}      # mapping from character to it's last seen index in s
+        start = 0           # start index of current substring
         longest = 0
 
         for i, c in enumerate(s):
-            if c in last_seen:
-                pass
+
+            if c in last_seen and last_seen[c] >= start:    # start a new substring after the previous sighting of c
+                start = last_seen[c]+1
             else:
+                longest = max(longest, i-start+1)
+
+            last_seen[c] = i    # update the last sighting index
+
+        return longest
