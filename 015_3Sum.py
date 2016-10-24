c35c7738505649bba1bbb74cfcfc779b8df0065a
Jake <jake_hoare@hotmail.com>
2016-10-24 19:21:54 +1100

    2 new solutions

---
+++ 015_3Sum.py
@@ -0,0 +1,54 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/3sum/
+# Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+# Find all unique triplets in the array which gives the sum of zero.
+# Note: The solution set must not contain duplicate triplets.
+
+# Sort the array.  For each index i perform a bidirectional search on the higher values in the array.
+# Skip over duplicates.  Increment i to the next new minimum number.
+# Time - O(n^2), for each i at least one of j and k moves every iteration.
+# Space - O(n)
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        results = []
+        nums.sort()
+
+        i = 0
+        while i < len(nums):
+            j = i + 1
+            k = len(nums) - 1
+
+            while j < k:
+
+                triple_sum = nums[i] + nums[j] + nums[k]
+
+                if triple_sum == 0:     # record result and move both j and k to next new numbers
+                    results.append([nums[i], nums[j], nums[k]])
+                    k -= 1
+                    while k > j and nums[k] == nums[k+1]:
+                        k -= 1
+                    j += 1
+                    while j < k and nums[j] == nums[j-1]:
+                        j += 1
+
+                elif triple_sum > 0:    # decrement k to next new number
+                    k -= 1
+                    while k > j and nums[k] == nums[k+1]:
+                        k -= 1
+                else:                   # increment j to next new number
+                    j += 1
+                    while j < k and nums[j] == nums[j-1]:
+                        j += 1
+
+            i += 1                      # increment i to next new number
+            while i < len(nums)-2 and nums[i] == nums[i-1]:
+                i += 1
+
+        return results
---
+++ 016_3Sum_Closest.py
@@ -0,0 +1,39 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/3sum-closest/
+# Given an array nums of n integers, find three integers in nums such that the sum is closest to a given number, target.
+# Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+# Sort the array. For each staring index bidirectional search in the rest of the array.
+# Time - O(n^2)
+# Space - O(1)
+
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        closest = float('inf')  # default if len(nums) < 3
+
+        for i in range(len(nums)-2):
+            j = i+1
+            k = len(nums)-1
+
+            while j < k:
+
+                triple = nums[i] + nums[j] + nums[k]
+                if triple == target:    # early return, cannot do better
+                    return target
+                if abs(triple-target) < abs(closest-target):
+                    closest = triple
+
+                if triple-target > 0:
+                    k -= 1
+                else:
+                    j += 1
+
+        return closest
