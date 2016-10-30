e6ac83aa6d7c82ec3823b9aaea0d09703767c823
Jake <jake_hoare@hotmail.com>
2016-10-31 09:18:43 +1100

    2 more

---
+++ 059_Spiral_Matrix_II.py
@@ -0,0 +1,33 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/spiral-matrix-ii/
+# Given an integer n, generate a square matrix filled with elements from 1 to n**2 in spiral order.
+
+# Change direction clockwise when edge of matrix or non-zero cell is reached.
+# Time - O(n**2)
+# Space - O(n**2)
+
+class Solution(object):
+    def generateMatrix(self, n):
+        """
+        :type n: int
+        :rtype: List[List[int]]
+        """
+        spiral = [[0 for _ in range(n)] for _ in range(n)]
+        row, col = 0, 0
+        d_r, d_c = 0, 1
+
+        count = 1
+        while count <= n*n:
+            spiral[row][col] = count
+            count += 1
+            if row+d_r < 0 or row+d_r >= n or col+d_c < 0 or col+d_c >= n or spiral[row+d_r][col+d_c] != 0:
+                d_r, d_c = d_c, -d_r
+            row += d_r
+            col += d_c
+
+        return spiral
+
+sol = Solution()
+print(sol.generateMatrix(5))
---
+++ 060_Permutation_Sequence.py
@@ -0,0 +1,39 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/permutation-sequence/
+# The set [1,2,3,…,n] contains a total of n! unique permutations.
+# By listing and labeling all of the permutations in order, we get the following sequence (ie, for n = 3):
+# "123", "132", "213", "231", "312", "321"
+# Given n and k, return the kth permutation sequence.
+
+# Find each digit according to the ratio between k and the total number of possible permutations.
+# Time - O(n**2) since each iteration adds one digit and O9n) for del from list
+# Space - O(n) to store chars list
+
+from math import factorial
+
+class Solution(object):
+    def getPermutation(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        chars = [str(i) for i in range(1, n+1)]     # the symbols that will be permuted
+        permutations = factorial(n)                 # total number of permutations for this n
+        k -= 1                                      # change indexing to 0
+        result = []
+
+        while chars:
+            digit = n * k // permutations           # get the first digit (range is 0 to n-1)
+            result.append(chars[digit])             # map from digit to a symbol
+            del chars[digit]                        # remove that symbol
+            permutations //= n                      # repeat for next digit with decreased permutations, n and k
+            k -= digit * permutations
+            n -= 1
+
+        return "".join(result)
+
+sol = Solution()
+print(sol.getPermutation(4,10))
