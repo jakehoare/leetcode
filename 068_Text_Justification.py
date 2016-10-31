b94bbb0f06d39fd89a3091c5785a6294b7c35785
Jake <jake_hoare@hotmail.com>
2016-11-01 10:15:03 +1100

    distribute spacing

--- 068_Text_Justification.py
+++ 068_Text_Justification.py
@@ -9,9 +9,9 @@ _project_ = 'leetcode'
 # For the last line of text, it should be left justified and no extra space is inserted between words.
 # Each word is guaranteed not to exceed L in length.
 
-# TODO SOLUTION
-# Time - O(TODO)
-# Space - O(TODO)
+# Fill lines with words and single spaces until full.  Then justify by distributing remaining spaces
+# Time - O(maxWidth * nb_lines)
+# Space - O(maxWidth * nb_lines)
 
 class Solution(object):
     def fullJustify(self, words, maxWidth):
@@ -20,4 +20,46 @@ class Solution(object):
         :type maxWidth: int
         :rtype: List[str]
         """
+        line_chars = 0          # number of chars on current line (without spaces)
+        line_words = []         # list of words on current line
+        justified = []          # output, list of justified strings
 
+        for word in words:
+
+            if line_chars + len(line_words) + len(word) <= maxWidth:    # add word to current line
+                line_words.append(word)
+                line_chars += len(word)
+
+            else:                                   # word cannot be added to current line
+                gaps = len(line_words) - 1          # nb gaps between words
+                spaces = maxWidth - line_chars      # nb of spaces to make line maxWidth
+                line = [line_words[0]]              # list of words and spaces
+                if gaps == 0:                       # pad end if single word
+                    line.append(" " * spaces)
+
+                for line_word in line_words[1:]:    # distribute spaces between other words
+                    space = spaces//gaps
+                    if spaces % gaps != 0:          # round up if uneven division of spaces
+                        space += 1
+                    line.append(" " * space)        # add space
+                    spaces -= space                 # reduce remaining spaces and gaps
+                    gaps -= 1
+                    line.append(line_word)
+
+                justified.append("".join(line))
+
+                line_words = [word]                 # add word to next line.
+                line_chars = len(word)
+
+        final_line = " ".join(line_words)           # pad final line with spaces
+        final_line += " " * (maxWidth - len(final_line))
+        justified.append(final_line)
+
+        return justified
+
+
+
+sol = Solution()
+test = ["Listen","to","many,","speak","to","a","few."]
+width = 6
+print(sol.fullJustify(test, width))
