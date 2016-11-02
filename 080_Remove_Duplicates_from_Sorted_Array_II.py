de1f5a57c0c2cfb299bf10e07b7fcb76c2defd54
Jake <jake_hoare@hotmail.com>
2016-11-03 10:07:20 +1100

    add if not 2 copies already

--- 080_Remove_Duplicates_from_Sorted_Array_II.py
+++ 080_Remove_Duplicates_from_Sorted_Array_II.py
@@ -1,12 +1,29 @@
 _author_ = 'jake'
 _project_ = 'leetcode'
 
-# https://leetcode.com/problems/TODO
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
 # Follow up for "Remove Duplicates":
 # What if duplicates are allowed at most twice?
 # For example, given sorted array nums = [1,1,1,2,2,3],
 # Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3.
 
-# TODO SOLUTION
-# Time - O(TODO)
-# Space - O(TODO)
+# Maintain a pointer to the next index to contain a result.  If a new number does not have 2 copies already
+# in result then add it.
+# Time - O(2)
+# Space - O(1)
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        next = 2    # next index to be filled with result
+
+        for index in range(2, len(nums)):
+
+            if nums[index] != nums[next-2]:     # result does not contain 2 copies of this num
+                nums[next] = nums[index]
+                next += 1
+
+        return min(next, len(nums))
