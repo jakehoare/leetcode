df6d3c15dac7ab87abe3314fcd2ac88c01196e9c
Jake <jake_hoare@hotmail.com>
2016-11-03 09:45:25 +1100

    DFS word search

--- 079_Word_Search.py
+++ 079_Word_Search.py
@@ -1,9 +1,47 @@
 _author_ = 'jake'
 _project_ = 'leetcode'
 
-# https://leetcode.com/problems/TODO
-# TODO PROBLEM DESCRIPTION
+# https://leetcode.com/problems/word-search/
+# Given a 2D board and a word, find if the word exists in the grid.
+# The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally
+# or vertically neighboring. The same letter cell may not be used more than once.
 
-# TODO SOLUTION
-# Time - O(TODO)
-# Space - O(TODO)
+# For each starting position, depth first search moving in all 4 directions and marking visited cells.
+# Time - O(m * n * s), for each starting board position, try upto s characters
+# Space - O(1)
+
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        if not board or not board[0]:
+            return False
+        rows, cols = len(board), len(board[0])
+
+        for r in range(rows):
+            for c in range(cols):
+                if self.can_find(word, 0, board, r, c):
+                    return True
+        return False
+
+
+    def can_find(self, word, i, board, r, c):
+
+        if i >= len(word):              # nothing more of word to find
+            return True
+        if r < 0 or r >= len(board) or c < 0 or c >= len(board[0]):     # outside board
+            return False
+        if word[i] != board[r][c]:      # no match letter
+            return False
+
+        board[r][c] = '*'               # set this position so cannot be used again
+
+        if (self.can_find(word, i+1, board, r+1, c) or self.can_find(word, i+1, board, r-1, c) or
+                self.can_find(word, i+1, board, r, c+1) or self.can_find(word, i+1, board, r, c-1)):
+            return True
+
+        board[r][c] = word[i]           # if False, reset position to letter
+        return False
---
+++ 080_Remove_Duplicates_from_Sorted_Array_II.py
@@ -0,0 +1,12 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/TODO
+# Follow up for "Remove Duplicates":
+# What if duplicates are allowed at most twice?
+# For example, given sorted array nums = [1,1,1,2,2,3],
+# Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3.
+
+# TODO SOLUTION
+# Time - O(TODO)
+# Space - O(TODO)
