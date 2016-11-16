4dd2c62fd748d32013ddbc169bd057b1b4c612e7
Jake <jake_hoare@hotmail.com>
2016-11-17 09:33:30 +1100

    traversals

---
+++ 144_Binary_Tree_Preorder_Traversal.py
@@ -0,0 +1,54 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/binary-tree-preorder-traversal/
+# Given a binary tree, return the preorder traversal of its nodes' values.
+
+# Maintain a stack of nodes discovered and to be visited.  Add right children to stack before left so left subtrees
+# are visited before right.
+# Alternatively, visit node, recurse left then recurse right.
+# Time - O(n)
+# Space - O(n)
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+
+        preorder = []
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            preorder.append(node.val)
+            if node.right:
+                stack.append(node.right)    # push right first so left is popped first
+            if node.left:
+                stack.append(node.left)
+
+        return preorder
+
+
+class Solution2(object):
+    def preorderTraversal(self, root):
+        result = []
+        self.preorder(root, result)
+        return result
+
+    def preorder(self, node, result):
+        if not node:
+            return
+        result.append(node.val)
+        self.preorder(node.left, result)
+        self.preorder(node.right, result)
---
+++ 145_Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,58 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/binary-tree-postorder-traversal/
+# Given a binary tree, return the postorder traversal of its nodes' values.
+
+# Maintain a stack of nodes discovered and to be visited.  Nodes are added to front of result.
+# Add left children to stack before right so right subtrees are visited before left.
+# Alternatively, recurse left, recurse right then visit node.
+# Time - O(n)
+# Space - O(n)
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+from collections import deque
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+
+        result = deque()        # deque instead of list so we can append at front
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            result.appendleft(node.val)
+
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+        return list(result)
+
+
+class Solution2(object):
+    def postorderTraversal(self, root):
+        result = []
+        self.postorder(root, result)
+        return result
+
+    def postorder(self, node, result):
+        if not node:
+            return
+        self.postorder(node.left, result)
+        self.postorder(node.right, result)
+        result.append(node.val)
+
