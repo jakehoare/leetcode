8e1684e785bbbf33d703c6c18f1668359da508a5
Jake <jake_hoare@hotmail.com>
2016-12-01 11:23:09 +1100

    typo

--- 219_Contains_Duplicate_II.java
+++ 219_Contains_Duplicate_II.java
@@ -3,7 +3,8 @@ https://leetcode.com/problems/contains-duplicate-ii/
 Given an array of integers and an integer k, find out whether there are two 
 distinct indices i and j in the array such that nums[i] = nums[j] 
 and the difference between i and j is at most k.
-** Store seen values in window, remove elements earlier than window.
+
+Store seen values in window, remove elements earlier than window.
 Time - O(n)
 Space - O(n)
  */
