f248812c75e3ab192674e75cd91c6a8df9e149d4
Jake <jake_hoare@hotmail.com>
2016-12-28 17:20:55 +1100

    typo

--- 237_Delete_Node_in_a_Linked_List.java
+++ 237_Delete_Node_in_a_Linked_List.java
@@ -2,7 +2,7 @@
 https://leetcode.com/problems/delete-node-in-a-linked-list/
 Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
 
-Copy value of next node then link to node aftre next and remove next node.
+Copy value of next node then link to node after next and remove next node.
 Time - O(1)
 Space - O(1)
 */
