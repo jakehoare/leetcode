f078b1cb64bbfc1ab8fd4b8d1f621f3131d18392
Jake <jake_hoare@hotmail.com>
2017-01-09 09:17:31 +1100

    recursive with memo

---
+++ 294_Flip_Game_II.py
@@ -0,0 +1,36 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/flip-game-ii/
+# You are playing the following Flip Game with your friend: Given a string that contains only these two characters:
+# + and -, you and your friend take turns to flip two consecutive "++" into "--". The game ends when a person can
+# no longer make a move and therefore the other person will be the winner.
+# Write a function to determine if the starting player can guarantee a win.
+
+# Find every '++' in s and if there is no winning strategy for opponent after converting it to '--' then this is a
+# winning strategy.
+# Time - O(n * n!), if string contains only '+' then n/2 choices for first pair to replace, (n-2)/2 for second pair ..
+# each replacement takes O(n)
+# Space - O(n * n!)
+
+class Solution(object):
+    def canWin(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        return self.helper(s, {})
+
+    def helper(self, s, memo):
+
+        if s in memo:
+            return memo[s]
+
+        for i in range(len(s)):
+            if s[i:i+2] == '++' and not self.helper(s[:i] + '--' + s[i+2:], memo):
+                memo[s] = True
+                return True
+
+        memo[s] = False
+        return False
+
