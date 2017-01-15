a86f254ca80e02ed8956f3379a6b92a0c30ee1ce
Jake <jake_hoare@hotmail.com>
2017-01-16 09:53:26 +1100

    one pass, array counts balance

--- 299_Bulls_and_Cows.java
+++ 299_Bulls_and_Cows.java
@@ -21,7 +21,6 @@ public class Solution {
     public String getHint(String secret, String guess) {
         int bulls = 0;
         int cows = 0;
-
         int digits[] = new int[10];     // balance of secret - guess (excluding bulls)
 
         for (int i = 0; i < secret.length(); ++i) {
