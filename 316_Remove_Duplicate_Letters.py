b5dcf92f81a31fd206b9d2e2e7cd73c040740a69
Jake <jake_hoare@hotmail.com>
2017-02-16 19:08:48 +1100

    typo

--- 316_Remove_Duplicate_Letters.py
+++ 316_Remove_Duplicate_Letters.py
@@ -1,7 +1,7 @@
 _author_ = 'jake'
 _project_ = 'leetcode'
 
-# https://leetcode.com/problems/remove-duplicate-letters
+# https://leetcode.com/problems/remove-duplicate-letters/
 # Given a string which contains only lowercase letters, remove duplicate letters so that every letter appear once
 # and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
 
