cbbd97c6b1e4cf604162a28c6c3338f5b1a52c63
Jake <jake_hoare@hotmail.com>
2017-03-05 10:31:29 +1100

    fix typo

--- 326_Power_of_Three.java
+++ 326_Power_of_Three.java
@@ -3,7 +3,7 @@ https://leetcode.com/problems/power-of-three/
 Given an integer, write a function to determine if it is a power of three.
 
 Find the largest power of 3 less than MAXINT.  If there is no remainder when this number is divided by n then n only
-containse factors of 3.
+contains factors of 3.
 Time - O(1)
 Space - O(1)
 */
