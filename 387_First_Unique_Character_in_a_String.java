d2c22276e2a6d1f54d00cd613d91722a683c24e4
jakehoare <jake_hoare@hotmail.com>
2017-04-26 18:57:07 +1000

    count letter frequencies

---
+++ 387_First_Unique_Character_in_a_String.java
@@ -0,0 +1,23 @@
+/*
+https://leetcode.com/problems/first-unique-character-in-a-string/
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Count frequencies of each letter.  Iterate over string again, return index of first letter with frequency of 1.
+Time - O(n)
+Space - O(1)
+*/
+
+public class Solution {
+    public int firstUniqChar(String s) {
+
+        int[] frequencies = new int[26];
+
+        for (int i = 0; i < s.length(); ++i)
+            ++frequencies[(int) s.charAt(i) - (int) 'a'];
+
+        for (int i = 0; i < s.length(); ++i)
+            if (frequencies[(int) s.charAt(i) - (int) 'a'] == 1)
+                return i;
+        return -1;
+    }
+}
