53420fea5c4688b87f378235b9fdc4e340629dc4
jakehoare <jake_hoare@hotmail.com>
2017-05-17 19:18:33 +1000

    Binary search range of max sums. Test if max sum is possible.

---
+++ 410_Split_Array_Largest_Sum.py
@@ -0,0 +1,43 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/split-array-largest-sum/
+# Given an array which consists of non-negative integers and an integer m, you can split the array into m non-empty
+# continuous subarrays. Write an algorithm to minimize the largest sum among these m subarrays.
+
+# Binary search the range of possible smallest max subarray sums. If x is valid, search range x and below else search
+# above x.
+# Time - O(n log n)
+# Space - O(1)
+
+class Solution(object):
+    def splitArray(self, nums, m):
+        """
+        :type nums: List[int]
+        :type m: int
+        :rtype: int
+        """
+        left, right = max(nums), sum(nums)  # search range
+
+        while left < right:  # if left == right then return
+            mid = (left + right) // 2
+            if self.can_split(nums, m, mid):  # recurse on LHS (smaller max subarray sums) including mid
+                right = mid     # mid < right so always make progress
+            else:
+                left = mid + 1
+
+        return left
+
+    def can_split(self, nums, m, max_subarray):  # can nums be split into m subarrays each sum <= max_subarray ?
+
+        subarray_sum = 0
+        for num in nums:
+            if num + subarray_sum > max_subarray:  # num makes sum too big
+                m -= 1  # one less subarray
+                if m <= 0:  # at least num remain with no more subarrays to use
+                    return False
+                subarray_sum = num  # num in next subarray
+            else:
+                subarray_sum += num
+
+        return True
