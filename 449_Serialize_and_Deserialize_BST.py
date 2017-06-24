b19a8d22846cfdc3c0384a7a4619ec0cb46a83cb
jakehoare <jake_hoare@hotmail.com>
2017-06-24 13:39:11 +1000

    Preorder serialise into space delimited string. Deserialise with deque. If first value is in specified range make root of tree and recurse.

---
+++ 449_Serialize_and_Deserialize_BST.py
@@ -0,0 +1,57 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/serialize-and-deserialize-bst/
+# Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your
+# serialization/deserialization algorithm should work. You just need to ensure that a binary search tree can be
+# serialized to a string and this string can be deserialized to the original tree structure.
+
+# Serialize by preorder traversal. String of values joined by spaces, no None markers.
+# Deserialze from deque. If first value is in range, use value as root and recurse left and right subtrees with ranges
+# less than and more than value respectively.
+# Time - O(n)
+# Space - O(n)
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+from collections import deque
+
+
+class Codec:
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+
+        def serial(node):
+            if not node:
+                return []
+            return [str(node.val)] + serial(node.left) + serial(node.right)
+
+        return " ".join(serial(root))
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        preorder = deque(int(val) for val in data.split())  # convert to integers
+
+        # if first preorder is between low and high, create tree of all vals in that range with first preorder as root
+        def deserial(low, high):
+            if preorder and low < preorder[0] < high:
+                val = preorder.popleft()
+                node = TreeNode(val)
+                node.left = deserial(low, val)
+                node.right = deserial(val, high)
+                return node
+
+        return deserial(float("-inf"), float("inf"))
