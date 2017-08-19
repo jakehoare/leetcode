fc4a742708ffd4158a891b893eeefe19e2f04204
jakehoare <jake_hoare@hotmail.com>
2017-08-20 09:04:53 +1000

    Stack of indices of nums without greater element. Iterate twice over nums.

---
+++ 503_Next_Grater_Element_II.py
@@ -0,0 +1,32 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/next-greater-element-ii/
+# Given a circular array (the next element of the last element is the first element of the array), print the Next
+# Greater Number for every element. The Next Greater Number of a number x is the first greater number to its
+# traversing-order next in the array, which means you could search circularly to find its next greater number.
+# If it doesn't exist, output -1 for this number.
+
+# Stack holds indices of numbers without greater element, hence decreasing order. Pop of all numbers lower than
+# current and set their next_greater to current. Repeat iteration over nums to account for circularity.
+# Time - O(n)
+# Space - O(n)
+
+class Solution(object):
+    def nextGreaterElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        stack = []                          # indices whose nums form decreasing sequence
+        next_greater = [-1] * len(nums)     # default -1 if no greater
+
+        for i in range(2 * len(nums)):
+
+            num = nums[i % len(nums)]       # wrap around
+            while stack and num > nums[stack[-1]]:
+                next_greater[stack.pop()] = num
+            if i < len(nums):
+                stack.append(i)
+
+        return next_greater
