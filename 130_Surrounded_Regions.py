d6fe562e90b664302e3445b37807d9a90cb62ee5
jakehoare <jake_hoare@hotmail.com>
2017-10-18 20:13:54 +1100

    Improve comments

--- 130_Surrounded_Regions.py
+++ 130_Surrounded_Regions.py
@@ -5,8 +5,8 @@ _project_ = 'leetcode'
 # Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
 # A region is captured by flipping all 'O's into 'X's in that surrounded region.
 
-# All edge cells containing 'O' adn their adjoining 'O' cannot be surrounded.  Temporarily flag these cell,
-# then convert all unflagged 'O' to 'X'.
+# All edge cells containing 'O' and their adjoining 'O' cannot be surrounded.  Temporarily flag these cells, by depth-
+# first search. Then convert all unflagged 'O' (that can be surrounded) to 'X'.
 # Time - O(m * n)
 # Space - O(m * n)
 
@@ -19,26 +19,21 @@ class Solution(object):
         if not board or not board[0]:
             return
 
-        rows = len(board)
-        cols = len(board[0])
+        rows, cols = len(board), len(board[0])
 
         to_expand = []              # stack of cells to be checked
 
         for row in range(rows):     # add all edge cells to stack
-            to_expand.append((row, 0))
-            to_expand.append((row, cols-1))
+            to_expand += [(row, 0), (row, cols - 1)]
         for col in range(1, cols-1):
-            to_expand.append((0, col))
-            to_expand.append((rows-1, col))
+            to_expand += [(0, col), (rows - 1, col)]
 
         while to_expand:            # if cell contains 'O', change to temporary 'T' and add neighbours to stack
             row, col = to_expand.pop()
             if 0 <= row < rows and 0 <= col < cols and board[row][col] == 'O':
                 board[row][col] = 'T'
-                to_expand.append((row+1, col))
-                to_expand.append((row-1, col))
-                to_expand.append((row, col+1))
-                to_expand.append((row, col-1))
+                for dr, dc in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                    to_expand.append((row + dr, col + dc))
 
         for row in range(rows):     # change all 'T' back to 'O' and all 'O' to 'X'
             for col in range(cols):
