0dab24895d573f7196910cc4e58ba9ed3ef43cd5
jakehoare <jake_hoare@hotmail.com>
2017-10-24 19:02:09 +1100

    update comments and correct time complexity

--- 113_Path_Sum_II.py
+++ 113_Path_Sum_II.py
@@ -4,10 +4,10 @@ _project_ = 'leetcode'
 # https://leetcode.com/problems/path-sum-ii/
 # Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
 
-# Maintain a partial path and either make a new copy with latest node (as in path_sum) or append latest node
-# and pop before returning (as in path_sum2, which is faster).
-# Time - O(n)
-# Space - O(n**2), every path of tree has correct sum
+# Maintain a partial path, appending latest node and decrementing target. When path has been explored, pop node off
+# partial path. Base cases are no node and leaf with remaining target of zero.
+# Time - O(n log n), balanced tree has n/2 leaves and height log n
+# Space - O(n log n), every path of balanced tree has correct sum
 
 # Definition for a binary tree node.
 class TreeNode(object):
@@ -24,22 +24,10 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         paths = []
-        self.path_sum(root, sum, [], paths)
+        self.preorder(root, sum, [], paths)
         return paths
 
-    def path_sum(self, node, target, partial, paths):
-
-        if not node:
-            return
-
-        target -= node.val
-        if target == 0 and not node.left and not node.right:
-            paths.append(partial + [node.val])
-
-        self.path_sum(node.left, target, partial + [node.val], paths)
-        self.path_sum(node.right, target, partial + [node.val], paths)
-
-    def path_sum2(self, node, target, partial, paths):
+    def preorder(self, node, target, partial, paths):
 
         if not node:
             return
@@ -49,7 +37,7 @@ class Solution(object):
         if target == 0 and not node.left and not node.right:
             paths.append(partial[:])
 
-        self.path_sum2(node.left, target, partial, paths)
-        self.path_sum2(node.right, target, partial, paths)
+        self.preorder(node.left, target, partial, paths)
+        self.preorder(node.right, target, partial, paths)
 
         partial.pop()
