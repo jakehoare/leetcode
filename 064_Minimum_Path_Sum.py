a600f0242e6ed241ebbe6cd55de738cee8375882
jakehoare <jake_hoare@hotmail.com>
2017-10-31 19:45:03 +1100

    Improve to O(n) space.

--- 064_Minimum_Path_Sum.py
+++ 064_Minimum_Path_Sum.py
@@ -8,8 +8,7 @@ _project_ = 'leetcode'
 
 # Dynamic programming, min path to a cell = cell value + min(min paths to cells above and left)
 # Time - O(m * n)
-# Space - O(m * n), could reduce to O(n) by keeping only previous row
-
+# Space - O(n)
 
 class Solution(object):
     def minPathSum(self, grid):
@@ -20,11 +19,13 @@ class Solution(object):
         m = len(grid)
         n = len(grid[0])
 
-        min_path = [[float('inf') for col in range(n+1)] for row in range(m+1)]
-        min_path[0][1] = 0
+        min_path = [float('inf') for _ in range(n + 1)]
+        min_path[1] = 0
 
-        for row in range(1, m+1):
-            for col in range(1, n+1):
-                min_path[row][col] = grid[row-1][col-1] + min(min_path[row-1][col], min_path[row][col-1])
+        for row in range(1, m + 1):
+            new_min_path = [float('inf') for _ in range(n + 1)]
+            for col in range(1, n + 1):
+                new_min_path[col] = grid[row - 1][col - 1] + min(min_path[col], new_min_path[col - 1])
+            min_path = new_min_path
 
-        return min_path[-1][-1]
+        return min_path[-1]
