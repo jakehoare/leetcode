a21e7eb846e1812706950c2f8df5153af7f93081
jakehoare <jake_hoare@hotmail.com>
2017-11-04 09:09:54 +1100

    Clarify code and comments

--- 164_Maximum_Gap.py
+++ 164_Maximum_Gap.py
@@ -6,8 +6,8 @@ _project_ = 'leetcode'
 # Return 0 if the array contains less than 2 elements.
 # You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
 
-# Bucket sort.
-# By pigeon-hole principle we ensure at least one bucket is empty (or all numbers in span are present).
+# Bucket sort. By pigeon-hole principle we ensure at least one bucket is empty or numbers are equally separated
+# by the same distance.
 # Time - O(n)
 # Space - O(n)
 
@@ -21,25 +21,29 @@ class Solution(object):
             return 0
 
         lower = min(nums)
-        span = max(nums) - lower + 1
-        if span < 2:                                # all same or 2 adjacent integers
-            return span-1
+        difference = max(nums) - lower
+        gaps = len(nums) - 1        # number of spaces between sorted nums
+        if difference == 0:         # all nums are same
+            return 0
 
-        nb_buckets = min(span, len(nums) + 1)       # ensure an empty bucket with len(nums)+1, flooring width at 1
-        width = span // nb_buckets                  # width of each bucket
-        if span % nb_buckets != 0:                  # round up so all of span is covered
-            width += 1
-        buckets = [[None, None] for _ in range(nb_buckets)]     # min and max of nums in each bucket
+        width = difference // gaps  # range of integers // number of gaps
+        if width == 0:
+            width = 1
 
-        for num in nums:                            # update min and max of corresponding bucket
+        nb_buckets = 1 + difference // width    # ensure max(nums) goes in final bucket
+
+        buckets = [[None, None] for _ in range(nb_buckets)] # max and min of each bucket
+
+        for num in nums:
             bucket = (num - lower) // width
-            buckets[bucket][0] = min(buckets[bucket][0], num) if buckets[bucket][0] else num
-            buckets[bucket][1] = max(buckets[bucket][1], num) if buckets[bucket][1] else num
+            buckets[bucket][0] = min(buckets[bucket][0], num) if buckets[bucket][0] != None else num
+            buckets[bucket][1] = max(buckets[bucket][1], num) if buckets[bucket][1] != None else num
 
-        last_used_bucket, max_gap = 0, 1            # default max gap to 1 if no gaps in buckets
-        for i in range(len(buckets)-1):
-            if not buckets[i][0] and buckets[i+1][0]:   # update max_gap if bucket is empty but next bucket is not
-                max_gap = max(max_gap, buckets[i+1][0] - buckets[last_used_bucket][1])
+        last_used_bucket = 0
+        max_gap = difference // gaps  # default case of no empty buckets
+        for i in range(nb_buckets - 1):
+            if not buckets[i][0] and buckets[i + 1][0]:
+                max_gap = max(max_gap, buckets[i + 1][0] - buckets[last_used_bucket][1])
             elif buckets[i][0]:
                 last_used_bucket = i
 
