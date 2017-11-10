5d65757022c1cc0e13043ac2f8a34424481c7301
jakehoare <jake_hoare@hotmail.com>
2017-11-11 10:52:20 +1100

    Improve comments.

--- 440_K-th_Smallest_in_Lexicographical_Order.py
+++ 440_K-th_Smallest_in_Lexicographical_Order.py
@@ -4,9 +4,9 @@ _project_ = 'leetcode'
 # https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/
 # Given integers n and k, find the lexicographically k-th smallest integer in the range from 1 to n.
 
-# Search range from and including lower to and excluding upper. Increase range by adding zeros while some part of range
-# is less than or equal to n. Then if range does not contain kth, skip past all count numbers and increment range start.
-# Else use kth number only and fix first digit.
+# Search integers beginning with 1, if not found increment first digit to 2, 3, ... etc.
+# Given a beginning digit kth, repeatedly multiply the start and end of search range by 10 until the range includes n.
+# If range does not include solution, increment beginning digit kth. Else use kth and multiply range start by 10.
 
 # Time - O(logn ** 2)
 # Space - O(1)
@@ -35,7 +35,7 @@ class Solution(object):
                 k -= count  # use all count numbers
                 kth += 1  # increment start point for next range
             else:
-                k -= 1  # use kth
+                k -= 1     # use kth
                 kth *= 10  # next range start
 
         return kth
