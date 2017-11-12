9b44a99972e3139a5d6119a80247d7420fcaf931
jakehoare <jake_hoare@hotmail.com>
2017-11-12 16:30:43 +1100

    Improve speed.

--- 169_Majority_Element.py
+++ 169_Majority_Element.py
@@ -5,9 +5,8 @@ _project_ = 'leetcode'
 # Given an array of size n, find the majority element. The majority element appears more than [n/2] times.
 # You may assume that the array is non-empty and the majority element always exist in the array.
 
-# In order for an element to be a majority element of the array it must also be the majority element of some
-# subarray.  Iterate over array creating subarrays assuming first element is majority.  Extend subarray to left, if
-# candidate is no longer in the majority then start a new subarray.
+# When the count is zero, the next element becomes the candidate. When an element is the same as the candidate,
+# increment the count, else decrement the count.
 # Time - O(n)
 # Space - O(1)
 
@@ -17,14 +16,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        count, start, candidate = 0, 0, None
+        count, candidate = 0, None
 
         for i, num in enumerate(nums):
-            if not candidate:       # start new subarray with first element as candidate
-                candidate = num
-            if candidate == num:    # increment count
-                count += 1
-            if count <= (i - start + 1) // 2:   # candidate is no longer in the majority
-                candidate = None
 
-        return candidate        # majority always exists so do not need to check candidate is majority of whole array
+            if count == 0:
+                candidate = num
+
+            if candidate == num:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
