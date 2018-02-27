fbf6394b1514c091bb68ec3a4919e76569d95fd1
jakehoare <jake_hoare@hotmail.com>
2018-02-27 19:07:40 +1100

    Improve comments.

--- 201_Bitiwse_AND_of_Numbers_Range.py
+++ 201_Bitiwse_AND_of_Numbers_Range.py
@@ -4,8 +4,10 @@ _project_ = 'leetcode'
 # https://leetcode.com/problems/bitwise-and-of-numbers-range/
 # Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
 
-# Starting from the highest bit that is set in n, whilst that bit is set ot the same value in both m and n then set it
-# to that value in the result.  Stop when a bit is set differently in m and n.
+# Starting from the most significant bit that is set in n, compare each bit in n to the equivalent bit in m.
+# If both bits are the same, set the same bit in the result to its value in m and n.
+# Stop when a bit is set differently in m and n.
+# When any bit is different, all less significant bits must take on both values of 0 and 1 in the range.
 # Time - O(log n)
 # Space - O(1)
 
