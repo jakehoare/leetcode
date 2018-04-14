8f563b814f4ce66f5737a004ac26531a3d677ede
jakehoare <jake_hoare@hotmail.com>
2018-04-14 14:56:30 +1000

    Find list of vowel indices. Iterate until midpoint, swapping first with last.

--- 345_Reverse_Vowels_of_a_String.py
+++ 345_Reverse_Vowels_of_a_String.py
@@ -4,8 +4,8 @@ _project_ = 'leetcode'
 # https://leetcode.com/problems/reverse-vowels-of-a-string/
 # Write a function that takes a string as input and reverse only the vowels of a string.
 
-# Start pointers from left adn right of s. Increment left pointer until a vowel is found. Decrement right pointer until
-# a vowel is found. Swap vowels. If pointers meet, return result.
+# Find list of indices of vowels in s. Swap vowel at first index with vowel at last index, until meeting in middle
+# of list of vowels.
 # Time - O(n)
 # Space - O(n)
 
@@ -16,24 +16,13 @@ class Solution(object):
         :rtype: str
         """
         vowels = {"a", "e", "i", "o", "u"}
-        vowels |= {c.upper() for c in vowels}   # add upper case vowels
-        s = [c for c in s]                      # convert to list of chars
-        left, right = 0, len(s) - 1
+        vowels |= {c.upper() for c in vowels}
 
-        while left < right:
+        vowel_i = [i for i, c in enumerate(s) if c in vowels]
+        n_vowel = len(vowel_i)
 
-            while s[left] not in vowels:
-                left += 1
-                if left == right:
-                    return "".join(s)
-
-            while s[right] not in vowels:
-                right -= 1
-                if left == right:
-                    return "".join(s)
-
-            s[left], s[right] = s[right], s[left]   # swap
-            left += 1
-            right -= 1
+        s = [c for c in s]
+        for j in range(n_vowel // 2):
+            s[vowel_i[j]], s[vowel_i[n_vowel - j - 1]] = s[vowel_i[n_vowel - j - 1]], s[vowel_i[j]]
 
         return "".join(s)
