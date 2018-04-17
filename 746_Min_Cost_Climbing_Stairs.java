19879d46e2eba215b7430b7b385de42fd2936177
jakehoare <jake_hoare@hotmail.com>
2018-04-17 18:38:21 +1000

    Amend.

--- 746_Min_Cost_Climbing_Stairs.java
+++ 746_Min_Cost_Climbing_Stairs.java
@@ -11,7 +11,7 @@ Space - O(1)
 
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        
+
         for (int i = cost.length - 3; i >= 0; --i)
             cost[i] += Math.min(cost[i + 1], cost[i + 2]);
 
