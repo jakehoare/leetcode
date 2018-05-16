bc40b3ad48b75f9df4a9ef74bb46c97115cb5819
jakehoare <jake_hoare@hotmail.com>
2018-05-17 06:10:21 +1000

    Tidy.

--- 286_Walls_and_Gates.py
+++ 286_Walls_and_Gates.py
@@ -8,8 +8,8 @@ _project_ = 'leetcode'
 #   INF - Infinity means an empty room. We use the value 2**31 - 1 = 2147483647 to represent INF.
 # Fill each empty room with the distance to its nearest gate. If it is impossible to reach a gate, it should be filled with INF.
 
-# Multidirectional BFS.  Frontier initially contains all gates.  For each room in frontier update any surrounding
-# rooms with infinite distance and add them to frontier.  Expands in concentric circles around gates.
+# Multidirectional BFS. Frontier initially contains all gates.  For each room in frontier update any surrounding
+# rooms with infinite distance and add them to frontier. Expands in concentric circles around gates.
 # DFS or BFS from each gate in turn can result in repeatedly updating rooms and greater time complexity.
 # Time - O(m * n), each cell visited once
 # Space - O(m * n) for queue
@@ -25,13 +25,14 @@ class Solution(object):
         if not rooms or not rooms[0]:
             return
 
-        INF = 2147483647
+        INF = 2 ** 31 - 1
         rows, cols = len(rooms), len(rooms[0])
-        frontier = deque([(r,c) for r in range(rows) for c in range(cols) if rooms[r][c]==0])
+        frontier = deque([(r, c) for r in range(rows) for c in range(cols) if rooms[r][c] == 0])
 
         while frontier:
             row, col = frontier.popleft()
-            for i, j in [(row+1, col), (row-1, col), (row, col+1), (row, col-1)]:
-                if 0 <= i < rows and 0 <= j < cols and rooms[i][j] == INF:
-                    rooms[i][j] = rooms[row][col]+1
-                    frontier.append((i, j))
+            for i, j in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
+                if i >= 0 and i < rows and j >= 0 and j < cols:
+                    if rooms[i][j] == INF:
+                        rooms[i][j] = rooms[row][col] + 1
+                        frontier.append((i, j))
