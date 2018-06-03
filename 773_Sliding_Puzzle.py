8a89c510e4d9e86217f0621cd2ae2403e5bf49b0
jakehoare <jake_hoare@hotmail.com>
2018-06-03 17:54:09 +1000

    Breadth-first search.

---
+++ 773_Sliding_Puzzle.py
@@ -0,0 +1,53 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/sliding-puzzle/
+# On a 2x3 board, there are 5 tiles represented by the integers 1 through 5, and an empty square represented by 0.
+# A move consists of choosing 0 and a 4-directionally adjacent number and swapping it.
+# The state of the board is solved if and only if the board is [[1,2,3],[4,5,0]].
+# Given a puzzle board, return the least number of moves required so that the state of the board is solved.
+# If it is impossible for the state of the board to be solved, return -1.
+
+# Breadth-first search. Expand queue from board by moving to all board positions after making a swap.
+# Alternatively, A* search with heap and heuristic of manhattan distance between each tile and its target location.
+# Time - O(mn * mn!) since there are mn! possible boards
+# Space - O(mn * mn!)
+
+class Solution(object):
+    def slidingPuzzle(self, board):
+        """
+        :type board: List[List[int]]
+        :rtype: int
+        """
+        nbors = [[1, 3], [0, 2, 4], [1, 5], [0, 4], [1, 3, 5], [2, 4]]  # indices of neighbouring cells in linear board
+
+        def next_boards(b):
+
+            i = b.index(0)
+
+            next_bds = []
+            for nbor in nbors[i]:
+                bd_copy = bd[:]             # make a copy
+                bd_copy[i], bd_copy[nbor] = bd_copy[nbor], bd_copy[i]   # swap zero with neighbour
+                next_bds.append(bd_copy)
+
+            return next_bds
+
+        seen = set()                        # convert board to tuple
+        steps = 0
+        queue = [board[0] + board[1]]       # convert board to linear board
+
+        while queue:
+
+            new_queue = []
+
+            for bd in queue:
+                if bd == [1, 2, 3, 4, 5, 0]:
+                    return steps
+                seen.add(tuple(bd))
+                new_queue += [nb for nb in next_boards(bd) if tuple(nb) not in seen]
+
+            steps += 1
+            queue = new_queue
+
+        return -1
