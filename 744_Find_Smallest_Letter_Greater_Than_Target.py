563ffe6d6ddf4e4685bf23e8aea79771500c458c
jakehoare <jake_hoare@hotmail.com>
2018-06-24 15:33:54 +1000

    Binary search.

---
+++ 744_Find_Smallest_Letter_Greater_Than_Target.py
@@ -0,0 +1,32 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+# Given a list of sorted characters letters containing only lowercase letters, and given a target letter target,
+# find the smallest element in the list that is larger than the given target.
+# Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+
+# Binary search for the next greater letter.
+# Time - O(log n)
+# Space - O(1)
+
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        left, right = 0, len(letters)   # initial search space is first letter to beyond last letter Inclusive)
+
+        while left < right:
+
+            mid = (left + right) // 2
+
+            if target >= letters[mid]:  # search RHS excluding mid
+                left = mid + 1
+            else:                       # mid could be next greater letter
+                right = mid
+
+        letters.append(letters[0])      # handle wrap around
+        return letters[left]
