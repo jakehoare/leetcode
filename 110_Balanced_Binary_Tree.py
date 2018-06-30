1ec4b692ef9398d6d465b0ecf8db5cddd8b04eb7
jakehoare <jake_hoare@hotmail.com>
2018-06-30 11:57:15 +1000

    Tidy.

--- 110_Balanced_Binary_Tree.py
+++ 110_Balanced_Binary_Tree.py
@@ -3,19 +3,14 @@ _project_ = 'leetcode'
 
 # https://leetcode.com/problems/balanced-binary-tree/
 # Given a binary tree, determine if it is height-balanced.
-# A height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+# A height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node
+# never differ by more than 1.
 
-# Calculate the depth of each node or -1 if not balanced.  Not balanced if either subtree not balanced or difference in
-# max subtree depths is more than 1.
+# Helper function balanced returns the depth of the root if the tree is balance or else -1 if not balanced.
+# A tree is not balance if either left or right subtree is not balanced, or the difference in left and right subtree
+# depths is greater than 1.
 # Time - O(n), visit all nodes
 # Space - O(n)
-
-# Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
 
 class Solution(object):
     def isBalanced(self, root):
@@ -23,20 +18,21 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-        return self.balanced(root) != -1
+        def balanced(node):
 
-    def balanced(self, node):
+            if not node:
+                return 0
 
-        if not node:
-            return 0
+            left_depth = balanced(node.left)
+            right_depth = balanced(node.right)
 
-        left_depth = self.balanced(node.left)
-        right_depth = self.balanced(node.right)
+            if left_depth == -1 or right_depth == -1:
+                return -1
+            if abs(left_depth - right_depth) > 1:
+                return -1
 
-        if left_depth == -1 or right_depth == -1:
-            return False
-        if abs(left_depth - right_depth) > 1:
-            return False
+            return 1 + max(left_depth, right_depth)
 
-        return 1 + max(left_depth, right_depth)
+        return balanced(root) != -1
+
 
