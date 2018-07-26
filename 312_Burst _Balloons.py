91ba91d941f49617c976f3b35f98000d15a83df5
jakehoare <jake_hoare@hotmail.com>
2018-07-27 08:07:30 +1000

    Improve comments.

--- 312_Burst _Balloons.py
+++ 312_Burst _Balloons.py
@@ -8,8 +8,8 @@ _project_ = 'leetcode'
 # Find the maximum coins you can collect by bursting the balloons wisely.
 
 # Dynamic programming.  Calculate the max coins for subarrays of increasing lengths by considering the max coins from
-# all possible balloons to be burst last.  Coins gained from bursting a balloon last is coins from that balloon *
-# cons from balloon before the subarray * coins from balloon after the subarray + coins from left subarray + coins
+# all possible balloons to be burst last.  Coins gained from bursting a balloon last = coins from that balloon *
+# coins from balloon before the subarray * coins from balloon after the subarray + coins from left subarray + coins
 # from right subarray.
 # Time - O(n**3)
 # Space - O(n**2)
@@ -22,16 +22,16 @@ class Solution(object):
         """
         n = len(nums)
         nums = [1] + nums + [1]
-        max_coins = [[0 for _ in range(n+2)] for _ in range(n+1)]   # row = length, col = left
+        max_coins = [[0 for _ in range(n + 2)] for _ in range(n + 1)]   # row = length, col = left
 
-        for length in range(1, n+1):
+        for length in range(1, n + 1):
 
-            for left in range(1, n+2-length):   # consider subarray from left to right inclusive
-                right = left+length-1
+            for left in range(1, n + 2 - length):   # consider subarrays from left to right inclusive
+                right = left + length - 1
 
-                for last in range(left, right+1):
-                    this_coins = nums[left-1] * nums[last] * nums[right+1]  # this balloon time those outside subarray
+                for last in range(left, right + 1):
+                    this_coins = nums[left - 1] * nums[last] * nums[right + 1]  # last balloon and neighbours
                     max_coins[length][left] = max(max_coins[length][left],
-                        this_coins + max_coins[last-left][left] + max_coins[right-last][last+1])
+                        this_coins + max_coins[last - left][left] + max_coins[right - last][last + 1])
 
         return max_coins[-1][1]
