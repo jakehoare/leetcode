05f3c37aeabb754c22ee5675778d94e62d3d738b
jakehoare <jake_hoare@hotmail.com>
2018-08-06 18:33:20 +1000

    Update comments.

--- 347_Top_K_Frequent_Elements.py
+++ 347_Top_K_Frequent_Elements.py
@@ -4,8 +4,8 @@ _project_ = 'leetcode'
 # https://leetcode.com/problems/top-k-frequent-elements/
 # Given a non-empty array of integers, return the k most frequent elements.
 
-# Count the frequency of each element.  Bucket sort the elements by frequency since the highest frequency cannot be
-# more than the length of nums.  Take the most frequent buckets, ties are split randomly.
+# Count the frequency of each element. Bucket sort the elements by frequency since the highest frequency cannot be
+# more than the length of nums. Take the most frequent buckets, ties are split randomly.
 # Time - O(n)
 # Space - O(n)
 
