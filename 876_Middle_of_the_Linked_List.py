dc1daa2bb96cb5cc401018406a436ef4dc5e6db1
jakehoare <jake_hoare@hotmail.com>
2018-09-22 11:48:25 +1000

    Fast and slow pointers.

---
+++ 876_Middle_of_the_Linked_List.py
@@ -0,0 +1,21 @@
+_author_ = 'jake'
+_project_ = 'leetcode'
+
+# https://leetcode.com/problems/middle-of-the-linked-list/
+# Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+# If there are two middle nodes, return the second middle node.
+
+# Move fast pointer for 2 steps and slow pointer for 1 step until fast foes not have a next next.
+# Time - O(n)
+# Space - O(1)
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow, fast = head, head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+        return slow
