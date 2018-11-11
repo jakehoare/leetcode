c5fb15df155ddd60b5015d57f86f5924c7bd11f0
jakehoare <jake_hoare@hotmail.com>
2018-11-11 15:06:50 +1100

    Update comments.

--- 115_Distinct_Subsequences.py
+++ 115_Distinct_Subsequences.py
@@ -6,9 +6,9 @@ _project_ = 'leetcode'
 # A subsequence of a string is a new string which is formed from the original string by deleting some (can be none)
 # of the characters without disturbing the relative positions of the remaining characters.
 
-# Dynamic programming.  For all prefixes of S and T calculate the number of sequences.  Base case is that the prefix
+# Dynamic programming. For all prefixes of S and T calculate the number of sequences. Base case is that the prefix
 # of T is the empty string, which is one subsequence of every prefix of S.
-# The number of time T[:r] is a subsequence of S[:c] is the number of time T[:r] is a subsequence of S[:c - 1], plus
+# The number of times T[:r] is a subsequence of S[:c] is the number of times T[:r] is a subsequence of S[:c - 1], plus
 # if the last chars of T[:r] and S[:c] match then every T[:r - 1] subsequence of S[:c] can also be extended.
 # empty string is a subsequence of any string.
 # Time - O(m * n) where m = len(S) and n = len(T)
