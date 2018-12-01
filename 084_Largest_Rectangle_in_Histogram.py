609d36eb227295b43b3a9ae9c0bb0871e33ec2da
jakehoare <jake_hoare@hotmail.com>
2018-12-01 12:27:31 +1100

    Improve code and comments.

--- 084_Largest_Rectangle_in_Histogram.py
+++ 084_Largest_Rectangle_in_Histogram.py
@@ -5,9 +5,10 @@ _project_ = 'leetcode'
 # Given n non-negative integers representing the histogram's bar height where the width of each bar is 1,
 # find the area of largest rectangle in the histogram.
 
-# Find the largest rectangle including each bar as the lowest bar.
-# An index is popped from the stack when a lower height is found.  We calculate the largest area with the popped
-# index.  Popped index is the lowest height, width is determined by i and the next index below on the stack.
+# For each bar, find the largest rectangle including that bar as the lowest bar.
+# An index is popped from the stack when a lower bar to the right is found.
+# We calculate the largest area with the bar at the popped index as the height (lowest bar in rectangle).
+# Width is determined by loses lower bar to the left and right.
 # Time - O(n)
 # Space - O(n)
 
@@ -18,22 +19,18 @@ class Solution(object):
         :rtype: int
         """
         max_area = 0
-        heights.append(0)   # added so last genuine index len(heights)-1 will be popped
-        stack = [0]         # top of stack is smallest, indices in decreasing height order
+        heights = [0] + heights + [0]   # stack will not be empt and last genuine bar will be popped
+        stack = [0]                     # indices of bars in non-decreasing height order
 
-        for i in range(1, len(heights)):    # heights[i] is right edge bar of the rectangle
+        for i, bar in enumerate(heights[1:], 1):
 
-            while stack and heights[i] < heights[stack[-1]]:    # pop bars lower than heights[i]
-                height = heights[stack.pop()]
-                if stack:
-                    width = i - stack[-1] - 1                   # top of stack is next higher bar on left
-                else:
-                    width = i
+            while heights[stack[-1]] > bar:     # pop taller off stack
+
+                height = heights[stack.pop()]   # form rectangle with popped bar determining height
+                width = i - stack[-1] - 1       # i and stack[-1] - 1 are the first lower bars on left and right
                 max_area = max(max_area, height * width)
 
             stack.append(i)
 
         return max_area
-
-
 
