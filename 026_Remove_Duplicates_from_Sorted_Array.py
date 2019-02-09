f17efca4819ce08429d8a728a34fb1c02ba7c8c3
jakehoare <jake_hoare@hotmail.com>
2019-02-10 07:58:17 +1100

    Update code and comments.

--- 026_Remove_Duplicates_from_Sorted_Array.py
+++ 026_Remove_Duplicates_from_Sorted_Array.py
@@ -5,8 +5,8 @@ _project_ = 'leetcode'
 # Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
 # Do not allocate extra space for another array, you must do this in place with constant memory.
 
-# Maintain a pointer to the next index to be filled with a new number.  Check every number against the previous num
-# (if any) and if different move to the nex_new index.
+# Maintain a pointer to the next index to be filled with a new number. Check every number against the previous num
+# (if any) and if different, move to the next_new index.
 # Time - O(n)
 # Space - O(1)
 
@@ -16,9 +16,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        next_new = 0
+        next_new = 0        # index where the next unique number is to be moved to
+
         for i in range(len(nums)):
-            if i == 0 or nums[i] != nums[i-1]:
+            if i == 0 or nums[i] != nums[i - 1]:
                 nums[next_new] = nums[i]
                 next_new += 1
+
         return next_new
