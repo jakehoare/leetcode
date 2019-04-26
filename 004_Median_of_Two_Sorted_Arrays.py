127ada498462f3b7cb282a0ea9b8811e91473bd2
jakehoare <jake_hoare@hotmail.com>
2019-04-26 18:57:47 +1000

    Update.

--- 004_Median_of_Two_Sorted_Arrays.py
+++ 004_Median_of_Two_Sorted_Arrays.py
@@ -11,39 +11,41 @@ _project_ = 'leetcode'
 # Space - O(log(m+n)) for the recursive call stack.
 
 class Solution(object):
-
     def findMedianSortedArrays(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: float
+        """
+        def get_kth_smallest(a_start, b_start, k):
 
-        right = self.get_kth_smallest(A, 0, B, 0, 1 + (len(A) + len(B))//2)
-        if (len(A) + len(B)) % 2 == 1:      # odd total length
+            if k <= 0 or k > len(A) - a_start + len(B) - b_start:
+                raise ValueError('k is out of the bounds of the input lists')
+
+            # base cases
+            if a_start >= len(A):
+                return B[b_start + k - 1]
+            if b_start >= len(B):
+                return A[a_start + k - 1]
+            if k == 1:
+                return min(A[a_start], B[b_start])
+
+            # remove k//2 elements from one list
+            # find the smallest of the k//2 - 1th element from both lists and recurse having reduced that list.
+            # k//2 - 1th element must exist in at least 1 list
+            mid_A, mid_B = float('inf'), float('inf')
+            if k // 2 - 1 < len(A) - a_start:
+                mid_A = A[a_start + k // 2 - 1]
+            if k // 2 - 1 < len(B) - b_start:
+                mid_B = B[b_start + k // 2 - 1]
+
+            if mid_A < mid_B:
+                return get_kth_smallest(a_start + k // 2, b_start, k - k // 2)
+            return get_kth_smallest(a_start, b_start + k // 2, k - k // 2)
+
+        right = get_kth_smallest(0, 0, 1 + (len(A) + len(B)) // 2)
+        if (len(A) + len(B)) % 2 == 1:  # odd total length
             return right
 
-        left = self.get_kth_smallest(A, 0, B, 0, (len(A) + len(B))//2)
+        left = get_kth_smallest(0, 0, (len(A) + len(B)) // 2)
         return (left + right) / 2.0
-
-
-    def get_kth_smallest(self, A, a_start, B, b_start, k):
-
-        if k <= 0 or k > len(A) - a_start + len(B) - b_start:
-            raise ValueError('k is out of the bounds of the input lists')
-
-        # base cases
-        if a_start >= len(A):
-            return B[b_start + k - 1]
-        if b_start >= len(B):
-            return A[a_start + k - 1]
-        if k == 1:
-            return min(A[a_start], B[b_start])
-
-        # remove k//2 elements from one list
-        # find the smallest of the k//2 - 1th element from both lists and recurse having reduced that list.
-        # k//2 - 1th element must exist in at least 1 list
-        mid_A, mid_B = float('inf'), float('inf')
-        if k//2 - 1 < len(A) - a_start:
-            mid_A = A[a_start + k//2 - 1]
-        if k//2 - 1 < len(B) - b_start:
-            mid_B = B[b_start + k//2 - 1]
-
-        if mid_A < mid_B:
-            return self.get_kth_smallest(A, a_start + k//2, B, b_start, k - k//2)
-        return self.get_kth_smallest(A, a_start, B, b_start + k//2, k - k//2)
