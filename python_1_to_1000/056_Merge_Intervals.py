a811a58f25d68a93158fb12c70cd4b2bf5231187
jakehoare <hjakeh@google.com>
2021-05-04 12:50:47 +1000

    Update for new definition of interval

--- python_1_to_1000/056_Merge_Intervals.py
+++ python_1_to_1000/056_Merge_Intervals.py
@@ -8,24 +8,18 @@ _project_ = 'leetcode'
 # Time - O(n log n)
 # Space - O(1)
 
-# Definition for an interval.
-class Interval(object):
-    def __init__(self, s=0, e=0):
-        self.start = s
-        self.end = e
-
 class Solution(object):
     def merge(self, intervals):
         """
         :type intervals: List[Interval]
         :rtype: List[Interval]
         """
-        intervals.sort(key=lambda x : x.start)
+        intervals.sort(key=lambda x : x[0])
         merged = []
 
         for interval in intervals:
-            if not merged or merged[-1].end < interval.start:
+            if not merged or merged[-1][1] < interval[0]:
                 merged.append(interval)
             else:
-                merged[-1].end = max(merged[-1].end, interval.end)
+                merged[-1][1] = max(merged[-1][1], interval[1])
         return merged
