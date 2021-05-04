a2c34939ed0eb71db325a7ec6dcb61de7835738b
jakehoare <hjakeh@google.com>
2021-05-04 12:45:45 +1000

    Update for new definition of interval

--- python_1_to_1000/057_Insert_Intervals.py
+++ python_1_to_1000/057_Insert_Intervals.py
@@ -11,28 +11,22 @@ _project_ = 'leetcode'
 # Time - O(n)
 # Space - O(1)
 
-# Definition for an interval.
-class Interval(object):
-    def __init__(self, s=0, e=0):
-        self.start = s
-        self.end = e
-
 class Solution(object):
     def insert(self, intervals, newInterval):
         """
-        :type intervals: List[Interval]
-        :type newInterval: Interval
-        :rtype: List[Interval]
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
         """
-        left, right = 0, len(intervals)-1
-        while left < len(intervals) and intervals[left].end < newInterval.start:
+        left, right = 0, len(intervals) - 1
+        while left < len(intervals) and intervals[left][1] < newInterval[0]:
             left += 1
 
-        while right >= 0 and intervals[right].start > newInterval.end:
+        while right >= 0 and intervals[right][0] > newInterval[1]:
             right -= 1
 
         if left <= right:
-            newInterval.start = min(newInterval.start, intervals[left].start)
-            newInterval.end = max(newInterval.end, intervals[right].end)
+            newInterval[0] = min(newInterval[0], intervals[left][0])
+            newInterval[1] = max(newInterval[1], intervals[right][1])
 
-        return intervals[:left] + [newInterval] + intervals[right+1:]
+        return intervals[:left] + [newInterval] + intervals[right + 1:]
